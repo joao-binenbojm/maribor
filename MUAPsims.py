a987c8219581ef3d8e4955ea2908de695f562a0f
joao-binenbojm <jbdsp.2@gmail.com>
2023-11-27 13:57:57 +0000

    sims results

--- MUAPsims.py
+++ MUAPsims.py
@@ -1,11 +1,19 @@
 from scipy.io import loadmat
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import os
 import numpy as np
+from time import time
 
-def sortMUAPs(data: np.ndarray, J: int):
+def eigen_cutoff(explained_var, thrs=0.99):
+    ''' Determines eigenvalue at which the cumulative sum of explained variance reaches a designated threshold.'''
+    cum_var = np.cumsum(explained_var)
+    loc = np.argwhere(cum_var >= thrs)[0,0] # first index where explained variance goes above threshold
+    return loc + 1 # eigenvalue at which explained variance goes above threshold
+
+def sortMUAPs(muaps: np.ndarray, J: int):
     ''' Given MUAPs data array, and extracts indices of MUAPs in ascending order of amplitude variance.'''
     print('SORTING MUAPs...')
     totvars = [] # variance of MUAP sizes across electrodes
@@ -19,15 +27,14 @@ def sortMUAPs(data: np.ndarray, J: int):
     MUranks = np.flip(np.argsort(avg_totvars)) # indices for sorting muaps based on MUAP size
     return MUranks
 
-def getH(path: str, K: int, J: int) -> np.ndarray:
+def getH(data: np.ndarray, K: int, J: int) -> np.ndarray:
     ''' Returns mixing matrix based on simulated MUAPS.'''
-    data = loadmat(path) # load data array
     Jmax = len(data['MUAPs']) # total number of motor units
     ncols = len(data['MUAPs'][0, 0][0]) # number of columns in simulated grid
     nrows, L = data['MUAPs'][0, 0][0, 0].shape # number of rows in simulated grid and length of MUAP
     M = ncols*nrows # total number of channels in the grid
     H = np.zeros((K*M, J*(L + K - 1))) # initialize mixing matrix
-    MUranks = sortMUAPs(data, J=J) # get indices of MUs sorted by average variability
+    MUranks = sortMUAPs(data['MUAPs'], J=J) # get indices of MUs sorted by average variability
 
     for jdx in range(J): # for each motor unit
         jrank = MUranks[jdx] # which motor unit to extract
@@ -38,40 +45,65 @@ def getH(path: str, K: int, J: int) -> np.ndarray:
                 H[mdx*K + kdx, interval+kdx:interval+kdx+L] = muap.ravel() # adding motor unit shapes to mixing matrix
     return H
 
-
 if __name__ == '__main__':
-    DIR = 'SynthSigs'
+    DIR = '../SynthSigs'
     name = 'SynthMUAP_BB_lib1_F5-5-5_Len20_ramp1_SNR10_20-Nov-2023_16_50'
-    data = loadmat(os.path.join(DIR, name))
-    # print(data.keys())
-    # force = data['Force']
-    # print(type(force))
-    # print(force.shape)
-    muaps = data['MUAPs']
-    # print(type(muaps))
-    # print(muaps.shape)
-    # fsamp = data['fsamp'][0,0]
+    muap_data = loadmat(os.path.join(DIR, name))
+    t0 = time()
 
-    H = getH(os.path.join(DIR, name), J=5)
-    nrows = H.shape[0]
-    max_amp = H.max()
-    plt.figure()
-    for idx in range(nrows):
-        plt.plot(H[idx, :] + nrows - 3*max_amp*idx)
-    plt.show()
+    # Run desired simulation with different parameters
+    data = [] # where to store each run
 
-    # Plotting covariance of H
-    cov = np.cov(H) # across rows
-    plt.figure()
-    sns.heatmap(cov)
-    plt.title('Covariance of simulated matrix')
-    plt.show()
+    for run in tqdm(range(1, 11)):
+        print('Run #{}...'.format(run)) 
+        for K in tqdm([40, 10, 1]):
+            print('For K = {}...'.format(K))
+            for J in np.arange(1, 152, 20):
 
-    # Plotting eigenspectrum of quadratic form of H
-    _, sigmas,_ = np.linalg.svd(H)
-    eigs = sigmas ** 2 # square singular vals to get eigenvalues
-    eigs = eigs / np.sum(eigs)
-    plt.figure()
-    plt.stem(eigs)
-    plt.title('Eigenspectrum of Simulated H')
-    plt.show()
+                    # Obtain singular values
+                    H = getH(muap_data, K=K, J=J)
+                    _, sigmas,_ = np.linalg.svd(H)
+                    eigs = sigmas ** 2 # square singular vals to get eigenvalues
+                    eigs = eigs / np.sum(eigs)
+
+                    # Compute eigen cutoffs
+                    eig1 =  eigen_cutoff(eigs, thrs=0.95) # accounting for 95% explained variance
+                    eig2 = eigen_cutoff(eigs, thrs=0.99) # accounting for 99% explained variance
+
+                    data.append([run, K, J, eig1, eig2])
+    
+    columns = ['Run', 'K', 'J', 'eig1', 'eig2']
+    df = pd.DataFrame(data=data, columns=columns) # store simulation results in a dataframe
+    df.to_csv('sims.csv')
+
+    # RUNTIME CALCULATION
+    tf = time()
+    print('Experiment took {} s'.format(tf - t0))
+    print('Experiment took {} min'.format((tf - t0)/60))
+    print('Experiment took {} h'.format((tf - t0)/3600))
+
+
+
+    # H = getH(os.path.join(DIR, name), J=5)
+    # nrows = H.shape[0]
+    # max_amp = H.max()
+    # plt.figure()
+    # for idx in range(nrows):
+    #     plt.plot(H[idx, :] + nrows - 3*max_amp*idx)
+    # plt.show()
+
+    # # Plotting covariance of H
+    # cov = np.cov(H) # across rows
+    # plt.figure()
+    # sns.heatmap(cov)
+    # plt.title('Covariance of simulated matrix')
+    # plt.show()
+
+    # # Plotting eigenspectrum of quadratic form of H
+    # _, sigmas,_ = np.linalg.svd(H)
+    # eigs = sigmas ** 2 # square singular vals to get eigenvalues
+    # eigs = eigs / np.sum(eigs)
+    # plt.figure()
+    # plt.stem(eigs)
+    # plt.title('Eigenspectrum of Simulated H')
+    # plt.show()
